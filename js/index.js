6859907f0c812c12b303a23b10d81daa489e046c
paola <paolatorobejar@gmail.com>
2017-03-13 14:17:56 -0500

    empezando js

--- js/index.js
+++ js/index.js
@@ -24,3 +24,32 @@ function paginaJuego()
 	$("section.inicio3").show();
 	$("section.inicio2").hide();
 }*/
+$(document).ready(init);
+function init()
+{	
+	var jugador1="x";
+	var jugador2="o";
+	var count=0;
+	
+	agregarValor();
+}
+function agregarValor()
+{
+	var divisiones=$(".divisiones").children();
+	alert(divisiones.length)
+}
+/*
+function agregarX()
+{
+	$(this).text("o");
+}
+
+function quienGano()
+{
+	var todos=$(".divisiones").children();
+	var posicion = ;
+	if(todos[1].value=="x" && todos[2].value=="x" && todos[3].value=="x")
+	{
+		alert("ganaste")
+	}
+}*/
